76ec4da59b45b748acb49db3df70b11cf396a28f
Ringo De Smet <ringo@de-smet.name>
2022-07-28 12:11:34 +0200

    Adding the security group rule to the standard setup
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- how-pulumi-works/index.ts
+++ how-pulumi-works/index.ts
@@ -11,15 +11,15 @@ const group = new aws.ec2.SecurityGroup(securityGroupName, {
     }
 });
 
-// const rule = new aws.ec2.SecurityGroupRule("http_access", {
-//     securityGroupId: group.id,
-//     description: "Enable HTTP access",
-//     type: "ingress",
-//     protocol: "tcp",
-//     fromPort: 80,
-//     toPort: 80,
-//     cidrBlocks: ["0.0.0.0/0"]
-// });
+const rule = new aws.ec2.SecurityGroupRule("http_access", {
+    securityGroupId: group.id,
+    description: "Enable HTTP access",
+    type: "ingress",
+    protocol: "tcp",
+    fromPort: 80,
+    toPort: 80,
+    cidrBlocks: ["0.0.0.0/0"]
+});
 
 // const server = new aws.ec2.Instance("web-server", {
 //     ami: "ami-0141514361b6a3c1b", // Ubuntu 20.04 Server - eu-west-1 - amd64
